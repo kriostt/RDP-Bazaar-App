d3875626b181d79c5646050de4e8ce56be15d03b
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-14 23:08:08 -0600

    Add Product entity

--- backend/src/main/java/RDP/Bazaar/backend/entity/Product.java
+++ backend/src/main/java/RDP/Bazaar/backend/entity/Product.java
@@ -1,4 +1,31 @@
 package RDP.Bazaar.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+// represents product
+@Entity // annotated as Entity for database persistence
+@Data // automatically generates getters and setters
+@NoArgsConstructor // automatically generates no argument constructor
+@AllArgsConstructor // automatically generates all arguments constructor
 public class Product {
+    // primary key for Product entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private String condition;
+
+    private Date datePosted;
 }
