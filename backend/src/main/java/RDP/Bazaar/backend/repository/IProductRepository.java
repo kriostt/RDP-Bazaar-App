b42582b6ce923db6b3d3180d4b3c9316873c792e
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-14 23:08:34 -0600

    Add logic for search and filter of products

--- backend/src/main/java/RDP/Bazaar/backend/repository/IProductRepository.java
+++ backend/src/main/java/RDP/Bazaar/backend/repository/IProductRepository.java
@@ -1,4 +1,34 @@
 package RDP.Bazaar.backend.repository;
 
-public interface IProductRepository {
+import RDP.Bazaar.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// manages Product entities
+@Repository // automatically detected and configured by Spring Data JPA
+public interface IProductRepository extends JpaRepository<Product, Long> {
+    // provides CRUD operations for Product entity
+
+    // search products by name or description
+    List<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
+
+    // filter products by condition
+    List<Product> findByConditionIgnoreCase(String condition);
+
+    // filter products by price range
+    List<Product> findByPriceBetween(double minPrice, double maxPrice);
+
+    // get all products sorted by price in ascending order
+    List<Product> findAllByOrderByPriceAsc();
+
+    // get all products sorted by price in descending order
+    List<Product> findAllByOrderByPriceDesc();
+
+    // get all products sorted by date posted in ascending order
+    List<Product> findAllByOrderByDatePostedAsc();
+
+    // get all products sorted by date posted in descending order
+    List<Product> findAllByOrderByDatePostedDesc();
 }
--- backend/src/main/java/RDP/Bazaar/backend/service/ProductService.java
+++ backend/src/main/java/RDP/Bazaar/backend/service/ProductService.java
@@ -1,4 +1,51 @@
 package RDP.Bazaar.backend.service;
 
+import RDP.Bazaar.backend.entity.Product;
+import RDP.Bazaar.backend.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// handles business logic related to Product entities
+@Service
 public class ProductService {
+    // automatic injection of IProductRepository instance
+    @Autowired
+    private IProductRepository productRepository;
+
+    // search products by name of description
+    public List<Product> search(String search) {
+        return productRepository.findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(search, search);
+    }
+
+    // filter products by condition
+    public List<Product> filterByCondition(String condition) {
+        return productRepository.findByConditionIgnoreCase(condition);
+    }
+
+    // filter products by price range
+    public List<Product> filterByPriceRange(double minPrice, double maxPrice) {
+        return productRepository.findByPriceBetween(minPrice, maxPrice);
+    }
+
+    // get all products sorted by price in ascending order
+    public List<Product> getAllProductsSortedByPriceAscending() {
+        return productRepository.findAllByOrderByPriceAsc();
+    }
+
+    // get all products sorted by price in descending order
+    public List<Product> getAllProductsSortedByPriceDescending() {
+        return productRepository.findAllByOrderByPriceDesc();
+    }
+
+    // get all products sorted by date posted in ascending order
+    public List<Product> getAllProductsSortedByDatePostedAscending() {
+        return productRepository.findAllByOrderByDatePostedAsc();
+    }
+
+    // get all products sorted by date posted in descending order
+    public List<Product> getAllProductsSortedByDatePostedDescending() {
+        return productRepository.findAllByOrderByDatePostedDesc();
+    }
 }
