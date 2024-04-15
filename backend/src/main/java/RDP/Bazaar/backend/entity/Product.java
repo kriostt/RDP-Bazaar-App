5d2df7fae67db11c1902cbdc9966bfaf4918dad2
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-15 01:59:22 -0600

    Update condition column name for Product entity

--- backend/src/main/java/RDP/Bazaar/backend/entity/Product.java
+++ backend/src/main/java/RDP/Bazaar/backend/entity/Product.java
@@ -1,9 +1,6 @@
 package RDP.Bazaar.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,6 +22,7 @@ public class Product {
     private String description;
     private double price;
     private String category;
+    @Column(name = "`condition`")
     private String condition;
 
     private Date datePosted;
