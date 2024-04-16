8cb9a03d16817837019eafb18114fe3fcaf228e9
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-16 11:49:43 -0600

    Add database interaction and logic for insights

--- backend/src/main/java/RDP/Bazaar/backend/repository/IProductRepository.java
+++ backend/src/main/java/RDP/Bazaar/backend/repository/IProductRepository.java
@@ -1,4 +1,33 @@
 package RDP.Bazaar.backend.repository;
 
-public interface IProductRepository {
+import RDP.Bazaar.backend.entity.Product;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// manages Product entities
+@Repository // automatically detected and configured by Spring Data JPA
+public interface IProductRepository extends JpaRepository<Product, Long> {
+    // JpaRepository<Product, Long> provides CRUD operations for Product entity
+
+    // increment the clicks of a product
+    @Transactional
+    @Modifying
+    @Query("UPDATE Product p SET p.clicks = p.clicks + 1 WHERE p.productId = :productId")
+    void incrementClicks(Long productId);
+
+    // get the total number of products that belong to a user
+    int countByUserUserId(Long userId);
+
+    // get the number of clicks for each product owned by a user
+    @Query("SELECT p, SUM(p.clicks) FROM Product p WHERE p.user.userId = :userId GROUP BY p")
+    List<Object[]> getClicksPerProductForUser(Long userId);
+
+    // get the overall number of clicks per date for all products that belong to a user
+    @Query("SELECT SUM(p.clicks), p.datePosted FROM Product p WHERE p.user.userId = :userId GROUP BY p.datePosted")
+    List<Object[]> getTotalClicksPerDateForUser(Long userId);
 }
--- backend/src/main/java/RDP/Bazaar/backend/service/ProductService.java
+++ backend/src/main/java/RDP/Bazaar/backend/service/ProductService.java
@@ -1,4 +1,35 @@
 package RDP.Bazaar.backend.service;
 
+import RDP.Bazaar.backend.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// handles business logic related to Product entities
+@Service
 public class ProductService {
+    // automatic injection of IProductRepository instance
+    @Autowired
+    private IProductRepository productRepository;
+
+    // increment number of clicks for specific product
+    public void incrementClicks(Long productId) {
+        productRepository.incrementClicks(productId);
+    }
+
+    // count total number of products that belong to a specific user
+    public int countProductsByUserId(Long userId) {
+        return productRepository.countByUserUserId(userId);
+    }
+
+    // get number of clicks per products for a specific user
+    public List<Object[]> getClicksPerProductForUser(Long userId) {
+        return productRepository.getClicksPerProductForUser(userId);
+    }
+
+    // get total number of clicks per date for all products that belong to a specific user
+    public List<Object[]> getTotalClicksPerDateForUser(Long userId) {
+        return productRepository.getTotalClicksPerDateForUser(userId);
+    }
 }
