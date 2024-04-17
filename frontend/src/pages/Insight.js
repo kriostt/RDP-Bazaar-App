197792e22f925c698540cb024990806ebfa3136b
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-17 03:33:34 -0600

    Add bar chart for clicks per product category

--- frontend/src/pages/Insight.js
+++ frontend/src/pages/Insight.js
@@ -1,15 +1,15 @@
 // import necessary modules
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "chartjs-adapter-date-fns";
-import { Chart as ChartJS } from "chart.js/auto";
-import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto"; // needed to render charts
+import { Doughnut, Bar } from "react-chartjs-2";
 
 const Insight = () => {
   // state variables to hold insight data
   const [productCount, setProductCount] = useState(0);
   const [clicksPerProduct, setClicksPerProduct] = useState([]);
   const [totalClicks, setTotalClicks] = useState([]);
+  const [clicksPerCategory, setClicksPerCategory] = useState([]);
 
   // function to fetch product count
   const fetchProductCount = async (userId) => {
@@ -59,6 +59,22 @@ const Insight = () => {
     }
   };
 
+  // function to fetch total clicks per category
+  const fetchClicksPerCategory = async () => {
+    try {
+      const response = await axios.get(
+        // send a GET request to getch clicks per category from backend
+        "http://localhost:9090/api/products/totalClicksPerCategory"
+      );
+
+      // update the state with the fetched response
+      setClicksPerCategory(response.data);
+    } catch (error) {
+      // handle errors if any occur during fetching
+      console.error("Error fetching clicks per category: ", error);
+    }
+  };
+
   // effect hook to fetch data when component mounts
   useEffect(() => {
     // !!! GET USER ID FOR LOGGED IN USER !!!
@@ -67,6 +83,7 @@ const Insight = () => {
     fetchProductCount(userId);
     fetchClicksPerProduct(userId);
     fetchTotalClicks(userId);
+    fetchClicksPerCategory();
   }, []);
 
   // data for doughnut chart displaying clicks per product
@@ -98,42 +115,108 @@ const Insight = () => {
     ],
   };
 
+  // data for bar chart displaying clicks per category
+  const clicksPerCategoryData = {
+    // category names
+    labels: clicksPerCategory.map((category) => category[0]),
+    datasets: [
+      {
+        label: "Clicks",
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 1,
+        borderRadius: 5,
+        // number of clicks per category
+        data: clicksPerCategory.map((category) => category[1]),
+      },
+    ],
+  };
+
   // JSX for insight component
   return (
-    <div
-      className="container d-flex justify-content-center align-items-center"
-      style={{ marginTop: "50px", width: "100%" }}
-    >
-      <div className="text-center">
-        {/* title for insight component */}
-        <h1 className="display-4 text-center mb-5">Insights</h1>
+    <div className="container mt-5">
+      <h1 className="display-4 text-center mb-5">Insights</h1>
 
-        {/* display total products */}
-        <p className="lead text-center">Total Products: {productCount}</p>
+      {/* display total products */}
+      <p className="lead text-center">Total Products: {productCount}</p>
 
-        {/* display total clicks */}
-        <p className="lead text-center">Total Clicks: {totalClicks}</p>
+      {/* display total clicks */}
+      <p className="lead text-center">Total Clicks: {totalClicks}</p>
 
-        <div className="card" style={{ width: "600px" }}>
-          <div className="card-body">
-            <h3 className="card-title text-center mb-3">Clicks Per Product</h3>
-            {/* render doughnut chart for clicks per product */}
-            <div style={{ width: "300px", margin: "0 auto" }}>
-              <Doughnut
-                data={clicksPerProductData}
-                options={{
-                  plugins: {
-                    legend: {
-                      position: "bottom",
-                      labels: {
-                        boxWidth: 20,
-                        fontSize: 12,
+      {/* container for doughtnut chart */}
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card mb-4 mx-3">
+            <div className="card-body">
+              {/* container title */}
+              <h3 className="card-title text-center mb-3">
+                Clicks Per Product
+              </h3>
+
+              <div
+                className="d-flex justify-content-center"
+                style={{ height: "300px" }}
+              >
+                {/* render doughnut chart */}
+                <Doughnut
+                  data={clicksPerProductData}
+                  options={{
+                    plugins: {
+                      legend: {
+                        position: "bottom",
+                        labels: {
+                          boxWidth: 20,
+                          fontSize: 12,
+                        },
                       },
                     },
-                  },
-                  aspectRatio: 1,
-                }}
-              />
+                    aspectRatio: 1,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* container for bar chart */}
+        <div className="col-md-6">
+          <div className="card mb-4 mx-3">
+            <div className="card-body">
+              {/* container title */}
+              <h3 className="card-title text-center mb-3">
+                Clicks Per Category
+              </h3>
+
+              <div
+                className="d-flex justify-content-center"
+                style={{ height: "300px" }}
+              >
+                {/* render bar chart */}
+                <Bar
+                  data={clicksPerCategoryData}
+                  options={{
+                    plugins: {
+                      legend: {
+                        display: false,
+                      },
+                    },
+                    scales: {
+                      x: {
+                        title: {
+                          display: true,
+                          text: "Categories",
+                        },
+                      },
+                      y: {
+                        title: {
+                          display: true,
+                          text: "Clicks",
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
