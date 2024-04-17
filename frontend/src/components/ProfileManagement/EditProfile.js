0734731207bd7e33c855cb54951737935bc5e068
devnexuss <ahavendicacion@gmail.com>
2024-04-16 19:59:25 -0600

    Profile Management Frontend Connected To Backend

--- frontend/src/components/ProfileManagement/EditProfile.js
+++ frontend/src/components/ProfileManagement/EditProfile.js
@@ -1,13 +1,15 @@
 import React from "react"; // Importing React hooks for managing component state
 import "../../App.css";
+import { useParams } from "react-router-dom"; // Importing useParams hook
 import ProfileManagement from "./ProfileManagement";
 
 // Edit Profile Page
 const EditProfile = () => {
+  const { userId } = useParams();
   return (
     <div className="App">
       <h2 className="profile-heading">My Profile</h2>
-      <ProfileManagement />
+      <ProfileManagement userId={userId} />
     </div>
   );
 };
