4d38831c98f6e6e3ae890b12b3190385e5f07be7
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-20 17:27:34 -0600

    Update SearchAndFilterServiceTest for full coverage

--- backend/src/test/java/RDP/Bazaar/backend/service/SearchAndFilterServiceTest.java
+++ backend/src/test/java/RDP/Bazaar/backend/service/SearchAndFilterServiceTest.java
@@ -39,7 +39,7 @@ public class SearchAndFilterServiceTest {
     }
 
     @Test
-    void searchAndFilterProductsTest_ShouldReturnSpecificProducts() {
+    void searchAndFilterProductsTest_SortByPriceAsc_ShouldReturnSpecificProductsOrderedByPriceAsc() {
         // create sample products
         Product product1 = new Product(
                 1,
@@ -106,7 +106,275 @@ public class SearchAndFilterServiceTest {
     }
 
     @Test
-    void searchAndFilterUsersTest_ShouldReturnSpecificUsers() {
+    void searchAndFilterProductsTest_SortByPriceDesc_ShouldReturnSpecificProductsOrderedByPriceDesc() {
+        // create sample products
+        Product product1 = new Product(
+                1,
+                "Product 1",
+                "Description 1",
+                10.00,
+                "Clothing",
+                "New",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product2 = new Product(
+                2,
+                "Product 2",
+                "Description 2",
+                20.00,
+                "Category 2",
+                "Condition 2",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product3 = new Product(
+                2,
+                "Product 3",
+                "Description 3",
+                30.00,
+                "Clothing",
+                "New",
+                new Date(),
+                0,
+                new User()
+        );
+
+        // create a list containing the sample products
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(product1);
+        expectedProducts.add(product3);
+
+        // mock the behaviour of productRepository to return list of expected products
+        given(productRepository.findAll(any(Specification.class))).willReturn(expectedProducts);
+
+        // test search and filter parameters
+        String search = "Product";
+        String category = "Clothing";
+        String productCondition = "New";
+        Double minPrice = 0.0;
+        Double maxPrice = 30.0;
+        String sortBy = "priceDesc";
+
+        // call the searchAndFilterProducts method
+        List<Product> actualProducts = searchAndFilterService.searchAndFilterProducts(
+                search, category, productCondition, minPrice, maxPrice, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 products after filtering
+        assertEquals(expectedProducts.size(), actualProducts.size());
+        // expecting Product 3 to be the first product
+        assertEquals("Product 3", actualProducts.get(0).getName());
+        // expecting Product 1 to be the second product
+        assertEquals("Product 1", actualProducts.get(1).getName());
+    }
+
+    @Test
+    void searchAndFilterProductsTest_SortByDatePostedAsc_ShouldReturnSpecificProductsOrderedByDatePostedAsc() {
+        // create sample products
+        Product product1 = new Product(
+                1,
+                "Product 1",
+                "Description 1",
+                10.00,
+                "Clothing",
+                "New",
+                new Date(2024, 4, 20),
+                0,
+                new User()
+        );
+        Product product2 = new Product(
+                2,
+                "Product 2",
+                "Description 2",
+                20.00,
+                "Category 2",
+                "Condition 2",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product3 = new Product(
+                2,
+                "Product 3",
+                "Description 3",
+                30.00,
+                "Clothing",
+                "New",
+                new Date(2024, 4, 21),
+                0,
+                new User()
+        );
+
+        // create a list containing the sample products
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(product1);
+        expectedProducts.add(product3);
+
+        // mock the behaviour of productRepository to return list of expected products
+        given(productRepository.findAll(any(Specification.class))).willReturn(expectedProducts);
+
+        // test search and filter parameters
+        String search = "Product";
+        String category = "Clothing";
+        String productCondition = "New";
+        Double minPrice = 0.0;
+        Double maxPrice = 30.0;
+        String sortBy = "datePostedAsc";
+
+        // call the searchAndFilterProducts method
+        List<Product> actualProducts = searchAndFilterService.searchAndFilterProducts(
+                search, category, productCondition, minPrice, maxPrice, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 products after filtering
+        assertEquals(expectedProducts.size(), actualProducts.size());
+        // expecting Product 1 to be the first product
+        assertEquals("Product 1", actualProducts.get(0).getName());
+        // expecting Product 3 to be the second product
+        assertEquals("Product 3", actualProducts.get(1).getName());
+    }
+
+    @Test
+    void searchAndFilterProductsTest_SortByDatePostedDesc_ShouldReturnSpecificProductsOrderedByDatePostedDesc() {
+        // create sample products
+        Product product1 = new Product(
+                1,
+                "Product 1",
+                "Description 1",
+                10.00,
+                "Clothing",
+                "New",
+                new Date(2024, 4, 20),
+                0,
+                new User()
+        );
+        Product product2 = new Product(
+                2,
+                "Product 2",
+                "Description 2",
+                20.00,
+                "Category 2",
+                "Condition 2",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product3 = new Product(
+                2,
+                "Product 3",
+                "Description 3",
+                30.00,
+                "Clothing",
+                "New",
+                new Date(2024, 4, 21),
+                0,
+                new User()
+        );
+
+        // create a list containing the sample products
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(product1);
+        expectedProducts.add(product3);
+
+        // mock the behaviour of productRepository to return list of expected products
+        given(productRepository.findAll(any(Specification.class))).willReturn(expectedProducts);
+
+        // test search and filter parameters
+        String search = "Product";
+        String category = "Clothing";
+        String productCondition = "New";
+        Double minPrice = 0.0;
+        Double maxPrice = 30.0;
+        String sortBy = "datePostedDesc";
+
+        // call the searchAndFilterProducts method
+        List<Product> actualProducts = searchAndFilterService.searchAndFilterProducts(
+                search, category, productCondition, minPrice, maxPrice, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 products after filtering
+        assertEquals(expectedProducts.size(), actualProducts.size());
+        // expecting Product 3 to be the first product
+        assertEquals("Product 3", actualProducts.get(0).getName());
+        // expecting Product 1 to be the second product
+        assertEquals("Product 1", actualProducts.get(1).getName());
+    }
+
+    @Test
+    void searchAndFilterProductsTest_SortByDefault_ShouldReturnSpecificProductsOrderedByProductId() {
+        // create sample products
+        Product product1 = new Product(
+                1,
+                "Product 1",
+                "Description 1",
+                10.00,
+                "Clothing",
+                "New",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product2 = new Product(
+                2,
+                "Product 2",
+                "Description 2",
+                20.00,
+                "Category 2",
+                "Condition 2",
+                new Date(),
+                0,
+                new User()
+        );
+        Product product3 = new Product(
+                2,
+                "Product 3",
+                "Description 3",
+                30.00,
+                "Clothing",
+                "New",
+                new Date(),
+                0,
+                new User()
+        );
+
+        // create a list containing the sample products
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(product1);
+        expectedProducts.add(product3);
+
+        // mock the behaviour of productRepository to return list of expected products
+        given(productRepository.findAll(any(Specification.class))).willReturn(expectedProducts);
+
+        // test search and filter parameters
+        String search = "Product";
+        String category = "Clothing";
+        String productCondition = "New";
+        Double minPrice = 0.0;
+        Double maxPrice = 30.0;
+        String sortBy = "";
+
+        // call the searchAndFilterProducts method
+        List<Product> actualProducts = searchAndFilterService.searchAndFilterProducts(
+                search, category, productCondition, minPrice, maxPrice, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 products after filtering
+        assertEquals(expectedProducts.size(), actualProducts.size());
+        // expecting Product 1 to be the first product
+        assertEquals("Product 1", actualProducts.get(0).getName());
+        // expecting Product 3 to be the second product
+        assertEquals("Product 3", actualProducts.get(1).getName());
+    }
+
+    @Test
+    void searchAndFilterUsersTest_SortByNameAsc_ShouldReturnSpecificUsersOrderedByNameAsc() {
         // create sample users
         User user1 = new User(
                 1,
@@ -159,6 +427,144 @@ public class SearchAndFilterServiceTest {
         // test search and filter parameters
         String search = "Josh";
         String sortBy = "nameAsc";
+
+        // call the searchAndFilterUsers method
+        List<User> actualUsers = searchAndFilterService.searchAndFilterUsers(
+                search, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 users after filtering
+        assertEquals(expectedUsers.size(), actualUsers.size());
+        // expecting Josh1 to be the first user
+        assertEquals("Josh1", actualUsers.get(0).getFirstName());
+        // expecting Josh3 to be the second user
+        assertEquals("Josh3", actualUsers.get(1).getFirstName());
+    }
+
+    @Test
+    void searchAndFilterUsersTest_SortByNameDesc_ShouldReturnSpecificUsersOrderedByNameDesc() {
+        // create sample users
+        User user1 = new User(
+                1,
+                "username1",
+                "Josh1",
+                "Lastname1",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+        User user2 = new User(
+                2,
+                "username2",
+                "Firstname2",
+                "Lastname2",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+        User user3 = new User(
+                3,
+                "username3",
+                "Josh3",
+                "Lastname3",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+
+        // create a list containing the sample users
+        List<User> expectedUsers = new ArrayList<>();
+        expectedUsers.add(user1);
+        expectedUsers.add(user3);
+
+        // mock the behaviour of userRepository to return list of expected users
+        given(userRepository.findAll(any(Specification.class))).willReturn(expectedUsers);
+
+        // test search and filter parameters
+        String search = "Josh";
+        String sortBy = "nameDesc";
+
+        // call the searchAndFilterUsers method
+        List<User> actualUsers = searchAndFilterService.searchAndFilterUsers(
+                search, sortBy
+        );
+
+        // --- assertions ---
+        // expecting 2 users after filtering
+        assertEquals(expectedUsers.size(), actualUsers.size());
+        // expecting Josh3 to be the first user
+        assertEquals("Josh3", actualUsers.get(0).getFirstName());
+        // expecting Josh1 to be the second user
+        assertEquals("Josh1", actualUsers.get(1).getFirstName());
+    }
+
+    @Test
+    void searchAndFilterUsersTest_SortByDefault_ShouldReturnSpecificUsersOrderedByUserId() {
+        // create sample users
+        User user1 = new User(
+                1,
+                "username1",
+                "Josh1",
+                "Lastname1",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+        User user2 = new User(
+                2,
+                "username2",
+                "Firstname2",
+                "Lastname2",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+        User user3 = new User(
+                3,
+                "username3",
+                "Josh3",
+                "Lastname3",
+                "",
+                "",
+                "",
+                "",
+                null,
+                null,
+                null
+        );
+
+        // create a list containing the sample users
+        List<User> expectedUsers = new ArrayList<>();
+        expectedUsers.add(user1);
+        expectedUsers.add(user3);
+
+        // mock the behaviour of userRepository to return list of expected users
+        given(userRepository.findAll(any(Specification.class))).willReturn(expectedUsers);
+
+        // test search and filter parameters
+        String search = "Josh";
+        String sortBy = "";
 
         // call the searchAndFilterUsers method
         List<User> actualUsers = searchAndFilterService.searchAndFilterUsers(
